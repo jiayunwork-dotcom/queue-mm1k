00fc2747c7d238bc3273b8ac4a8ff659c9dd6387
jiayun <jiayun_work@163.com>
2026-08-20 16:30:33 +0800

    chore: plant bug (JYY-0334)

---
+++ internal/analytic/rhodrop.go
@@ -0,0 +1,12 @@
+package analytic
+
+func dropRho(r float64) float64 {
+	return 0
+}
+
+func applyRho(m *MMC) float64 {
+	if m == nil || m.Mu == 0 {
+		return 0
+	}
+	return dropRho(m.Lambda / m.Mu)
+}
